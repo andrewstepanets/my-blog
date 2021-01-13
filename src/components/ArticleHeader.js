6f81de2fde960494fd3e494ca5243d1e67b27a90
Andrii Stepanets <ovrass@gmail.com>
2021-01-13 18:18:38 +0200

    Added blog detail page layout

---
+++ src/components/ArticleHeader.js
@@ -0,0 +1,23 @@
+
+
+export default function ArticleHeader({ title, subtitle, image, author }) {
+    return (
+        <div className="blog-detail-header">
+            <p className="lead mb-0">
+                <img
+                    src={author?.avatar}
+                    className="rounded-circle mr-3"
+                    height="50px"
+                    width="50px"
+                    alt="avatar" />
+                {`${author?.name}, ${author?.date}`}
+            </p>
+            <h1 className="font-weight-bold blog-detail-header-title mb-0">{title}</h1>
+            <h2 className="blog-detail-header-subtitle mb-3">{subtitle}</h2>
+            <img
+                className="img-fluid rounded"
+                src={image} alt={title} />
+
+        </div>
+    )
+}
--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,6 +1,8 @@
 
 import Layout from 'components/Layout'
+import { Row, Col } from 'react-bootstrap'
 import { getBlogBySlug, getAllBlogs } from '../../../lib/api'
+import ArticleHeader from 'components/ArticleHeader'
 
 export async function getStaticPaths() {
     const blogs = await getAllBlogs();
@@ -20,10 +22,20 @@ export async function getStaticProps({ params }) {
 
 
 function BlogDetail({ blog }) {
-    console.log(blog);
     return (
-        <Layout>
-            <h1>Details Page - {blog?.slug}</h1>
+        <Layout className="blog-detail-page">
+            <Row>
+                <Col md={{ span: 10, offset: 1 }}>
+                    <ArticleHeader
+                        title={blog.title}
+                        subtitle={blog.subtitle}
+                        image={blog.coverImage}
+                        author={blog.author}
+                    />
+                    <hr />
+      It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    </Col>
+            </Row>
         </Layout>
     )
 }
