d3bd58c4b990c6319ec146c9c8d6b1eb9238afe9
Andrii Stepanets <ovrass@gmail.com>
2021-01-13 17:48:42 +0200

    Change to getStaticPaths func

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,14 +1,23 @@
 
 import Layout from 'components/Layout'
-import { getBlogBySlug } from '../../../lib/api'
+import { getBlogBySlug, getAllBlogs } from '../../../lib/api'
 
+export async function getStaticPaths() {
+    const blogs = await getAllBlogs();
+    return {
+        paths: blogs?.map(blog => ({ params: { slug: blog.slug } })),
+        fallback: false
+    }
+}
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
     const blog = await getBlogBySlug(params.slug);
     return {
         props: { blog }
     }
 }
+
+
 
 function BlogDetail({ blog }) {
     console.log(blog);
